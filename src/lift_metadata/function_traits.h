c8a551411eae2e75f77bb503852fd945d85fe47f
Phil Egelston <pegelston@gmail.com>
2014-11-02 15:53:28 -0600

    Add VisualStudio project extensions to the ignored list. Add function_traits to CMakeLists and list_metadata project.

---
+++ src/lift_metadata/function_traits.h
@@ -0,0 +1,32 @@
+
+#include <tuple>
+
+//
+// This template allows the deduction of parameter and return type
+// of functions. This includes lambdas and specilizations for methods.
+//
+
+template <typename T>
+struct function_traits : 
+    public function_traits<decltype(&T::operator())>
+{
+    // For generic types, directly use the result of the signature of its 'operator()'
+};
+
+// we specialize for pointers to member function
+template <typename ClassType, typename ReturnType, typename... Args>
+struct function_traits<ReturnType(ClassType::*)(Args...) const>
+{
+    // arity is the number of arguments.
+    enum { arity = sizeof...(Args) };
+
+    typedef ReturnType result_type;
+
+    template <size_t i>
+    struct arg
+    {
+        // the i-th argument is equivalent to the i-th tuple 
+        // element of a tuple composed of those arguments.
+        typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+    };
+};
